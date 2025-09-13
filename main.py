c8879cefaec3ea53b2e2f29556d58cc2d6d3ef92
bpolly06 <bpolly88@gmail.com>
2025-09-13 15:51:17 -0400

    added wandb.config to main.py

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from omegaconf import DictConfig, OmegaConf
 @hydra.main(config_name='config')
 def go(config: DictConfig):
 
+    wandb.config = OmegaConf.to_container(
+         config, 
+         resolve=True, 
+         throw_on_missing=True
+    )
+
     # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
